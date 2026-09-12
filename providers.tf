0becedd867fe73b87d4dda929777224e4b7d7c79
Samir Adhikari <sadhikaril@netskope.com>
2026-09-12 20:07:59 +0545

    feat: add s3 storage

---
+++ providers.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+
+  endpoints {
+    s3 = "http://localhost.floci.io:4566"
+  }
+}
