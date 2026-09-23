b9ed4cc8e3c5869b883f892c7c3db99cf6432a07
Samir Adhikari <sadhikaril@netskope.com>
2026-09-23 20:12:20 +0545

    modify providers configuration

--- providers.tf
+++ providers.tf
@@ -10,9 +10,19 @@ terraform {
 provider "aws" {
   region = "us-east-1"
 
+  access_key = "test"
+  secret_key = "test"
+
+  skip_credentials_validation = true
+  skip_metadata_api_check     = true
+  skip_requesting_account_id  = true
+
   endpoints {
-    s3     = "http://localhost.floci.io:4566"
-    sqs    = "http://localhost.floci.io:4566"
-    lambda = "http://localhost.floci.io:4566"
+    s3       = "http://localhost.floci.io:4566"
+    sqs      = "http://localhost.floci.io:4566"
+    dynamodb = "http://localhost.floci.io:4566"
+    sts      = "http://localhost.floci.io:4566"
   }
+
+  s3_use_path_style = true
 }
